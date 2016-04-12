3d87389f44d20e8423759187968b92e0fb95d5f7
Aleksey2093 <Aleksey2093@outlook.com>
2016-04-12 23:07:06 +0300

    Update GetFriendsLastResult.java
    Механизм включения и выключения сокета прослушки (по факту уничтожение потока, который этим сокетом занимался), чтобы не было тупиков.

--- src/main/java/aleksey2093/GetFriendsLastResult.java
+++ src/main/java/aleksey2093/GetFriendsLastResult.java
@@ -23,21 +23,35 @@ public class GetFriendsLastResult {
         Socket soc = null;
         DataOutputStream outputStream;
         DataInputStream inputStream;
-        try {
-            soc = new Socket(giveMeSettings.getServerName(false), giveMeSettings.getServerPort(false));
-            outputStream = new DataOutputStream(soc.getOutputStream());
-            inputStream = new DataInputStream(soc.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return list;
+        while (true) {
+            try {
+                soc = new Socket(giveMeSettings.getServerName(false), giveMeSettings.getServerPort(false));
+                outputStream = new DataOutputStream(soc.getOutputStream());
+                inputStream = new DataInputStream(soc.getInputStream());
+                break;
+            } catch (IOException e) {
+                /* выключаем сокет прослушки, если он мешает (использует необходимый порт) */
+                if (!ListenResultFromServer.serversock.isClosed()) {
+                    try {
+                        ListenResultFromServer.serversock.close();
+                        ListenResultFromServer.thread.stop();
+                        continue;
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                e.printStackTrace();
+                return null;
+            }
         }
         byte[] login = giveMeSettings.getLpk((byte) 1);
         byte[] pass = giveMeSettings.getLpk((byte) 2);
         //байт шифр, байт тип, байт длинны логина, логин, байт длинны пароля, пароль, логин друга (чей результат будем смотреть)
         byte[] msg = new byte[1 + 1 + 1 + login.length + 1 + pass.length + loginfriend.getBytes().length];
         msg[0] = giveMeSettings.getEncryption();
-        msg[1] = 3; int j = 3;
+        msg[1] = 3;
         msg[2] = (byte) login.length;
+        int j = 3;
         for (int i=0;i<login.length;i++,j++)
             msg[j]=login[i];
         msg[j] = (byte) pass.length; j++;
@@ -49,7 +63,7 @@ public class GetFriendsLastResult {
             outputStream.write(msg);
         } catch (IOException e) {
             e.printStackTrace();
-            return list;
+            return null;
         }
         int len = 0;
         while (len <= 0) {
@@ -62,21 +76,23 @@ public class GetFriendsLastResult {
         }
         try {
             soc.close();
+            ListenResultFromServer.thread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
         j=2;
         //дешефруем тут
         if (msg[1] != 2 && msg[1] != 3) {
-            return list;
+            return null;
         }
         try {
             while (j < len) {
                 int linklen = ByteBuffer.wrap(msg, j, 4).getInt(); j += 4;
                 list.add(new String(msg, j, linklen, "UTF-8")); j+=linklen;
             }
+        return list;
         }
         catch (Exception ex) { ex.printStackTrace(); }
-        return list;
+        return null;
     }
 }
