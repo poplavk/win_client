dceca628b8fef0cfabab2b6a4f819a481402b1af
Aleksey2093 <Aleksey2093@outlook.com>
2016-05-09 15:27:32 +0300

    Update GetFriendsLastResult.java

--- src/main/java/aleksey2093/GetFriendsLastResult.java
+++ src/main/java/aleksey2093/GetFriendsLastResult.java
@@ -1,15 +1,14 @@
 package aleksey2093;
 
+import javafx.scene.control.Alert;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
-import java.net.SocketOptions;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Exchanger;
 
 /**
  * Created by aleks on 10.04.2016.
@@ -17,54 +16,62 @@ import java.util.concurrent.Exchanger;
 public class GetFriendsLastResult {
 
     //возвращает лист подписчиков при выборе подписчика в списке
-    public ArrayList getLastResult(String loginfriend) {
+    public ArrayList<String> getLastResult(String friend) {
         ArrayList<String> list = new ArrayList<String>();
         GiveMeSettings giveMeSettings = new GiveMeSettings();
-        Socket soc = null;
+        Socket socket = null;
         DataOutputStream outputStream;
         DataInputStream inputStream;
-        while (true) {
-            try {
-                soc = new Socket(giveMeSettings.getServerName(false), giveMeSettings.getServerPort(false));
-                outputStream = new DataOutputStream(soc.getOutputStream());
-                inputStream = new DataInputStream(soc.getInputStream());
-                break;
-            } catch (IOException e) {
-                /* выключаем сокет прослушки, если он мешает (использует необходимый порт) */
-                if (!ListenResultFromServer.serversock.isClosed()) {
-                    try {
-                        ListenResultFromServer.serversock.close();
-                        ListenResultFromServer.thread.stop();
-                        continue;
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                    }
-                }
-                e.printStackTrace();
-                return null;
-            }
-        }
-        byte[] login = giveMeSettings.getLpk((byte) 1);
-        byte[] pass = giveMeSettings.getLpk((byte) 2);
-        //байт шифр, байт тип, байт длинны логина, логин, байт длинны пароля, пароль, логин друга (чей результат будем смотреть)
-        byte[] msg = new byte[1 + 1 + 1 + login.length + 1 + pass.length + loginfriend.getBytes().length];
-        msg[0] = giveMeSettings.getEncryption();
-        msg[1] = 3;
-        msg[2] = (byte) login.length;
-        int j = 3;
-        for (int i=0;i<login.length;i++,j++)
-            msg[j]=login[i];
-        msg[j] = (byte) pass.length; j++;
-        for (int i=0;i<pass.length;i++,j++)
-            msg[j]=login[i];
-        for (int i=0;i<loginfriend.getBytes().length;i++,j++)
-            msg[j]=loginfriend.getBytes()[i];
         try {
-            outputStream.write(msg);
+            socket = new Socket(giveMeSettings.getServerName(2), giveMeSettings.getServerPort(2));
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            inputStream = new DataInputStream(socket.getInputStream());
         } catch (IOException e) {
+                /* выключаем сокет прослушки, если он мешает (использует необходимый порт) */
             e.printStackTrace();
             return null;
         }
+        if (sendRequestToServer(giveMeSettings, outputStream, friend)) {
+            list = waitServerMsg(inputStream, friend);
+        }
+        if (!socket.isClosed())
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return list;
+    }
+
+    private boolean sendRequestToServer(GiveMeSettings giveMeSettings, DataOutputStream outputStream, String friend) {
+        try {
+            byte[] login = giveMeSettings.getLpk((byte) 1);
+            byte[] pass = giveMeSettings.getLpk((byte) 2);
+            //байт шифр, байт тип, байт длинны логина, логин, байт длинны пароля, пароль, логин друга (чей результат будем смотреть)
+            byte[] msg = new byte[1 + 1 + 1 + login.length + 1 + pass.length + friend.getBytes().length];
+            msg[0] = giveMeSettings.getEncryption();
+            msg[1] = 3;
+            msg[2] = (byte) login.length;
+            int j = 3;
+            for (int i = 0; i < login.length; i++, j++)
+                msg[j] = login[i];
+            msg[j] = (byte) pass.length;
+            j++;
+            for (int i = 0; i < pass.length; i++, j++)
+                msg[j] = login[i];
+            for (int i = 0; i < friend.getBytes().length; i++, j++)
+                msg[j] = friend.getBytes()[i];
+            outputStream.write(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private ArrayList<String> waitServerMsg(DataInputStream inputStream, String friend) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        byte[] msg = new byte[1];
         int len = 0;
         while (len <= 0) {
             try {
@@ -72,27 +79,64 @@ public class GetFriendsLastResult {
                 len = inputStream.read(msg);
             } catch (IOException e) {
                 e.printStackTrace();
+                return null;
             }
         }
-        try {
-            soc.close();
-            ListenResultFromServer.thread.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        j=2;
-        //дешефруем тут
-        if (msg[1] != 2 && msg[1] != 3) {
+        //дешифруем полученное сообщение
+        if (msg[1] == (byte) 103) {
+            showDialogInform(true, null);
+            return null;
+        } else if (msg[1] != 3) {
+            return null;
+        } else if (len <= 3) {
+            showDialogInform(false, friend);
             return null;
         }
-        try {
-            while (j < len) {
-                int linklen = ByteBuffer.wrap(msg, j, 4).getInt(); j += 4;
-                list.add(new String(msg, j, linklen, "UTF-8")); j+=linklen;
+        formationListLinks(msg, len, friend);
+        return arrayList;
+    }
+
+    private ArrayList<String> formationListLinks(byte[] msg, int len, String friend) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        int i = 2;
+        while (i < len) {
+            int lenlink = ByteBuffer.wrap(msg, i, 4).getInt();
+            i += 4;
+            try {
+                String link = new String(msg, i, lenlink, "UTF-8");
+                link = getIdFromLink(link);
+                if (link != null)
+                    arrayList.add(link);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             }
-        return list;
+            i += lenlink;
         }
-        catch (Exception ex) { ex.printStackTrace(); }
-        return null;
+        return arrayList;
+    }
+
+    private String getIdFromLink(String link) {
+        String tmp = null;
+        if (link.toCharArray()[link.length() - 1] == '/')
+            link = link.substring(0, link.length() - 2);
+        for (int i = link.length() - 1; i >= 0; i--) {
+            tmp = link.toCharArray()[i] + tmp;
+        }
+        return tmp;
+    }
+
+    private void showDialogInform(boolean what, String string) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Информация");
+        if (what) {
+            System.out.println("Неправильный логин или пароль.");
+            alert.setHeaderText("Ошибка входа");
+            alert.setContentText("Неправильный логин или пароль");
+        } else {
+            System.out.println("Последний результат " + string + "пуст");
+            alert.setHeaderText("Пустой результат");
+            alert.setContentText("Результат " + string + " оказался пуст");
+        }
+        alert.showAndWait();
     }
 }
