b5d2a79c6c6068f30585e2498922f41a4f061f37
Aleksey2093 <Aleksey2093@outlook.com>
2016-05-09 15:28:04 +0300

    Update ListenResultFromServer.java

--- src/main/java/aleksey2093/ListenResultFromServer.java
+++ src/main/java/aleksey2093/ListenResultFromServer.java
@@ -5,6 +5,8 @@ import javafx.scene.control.ButtonType;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
@@ -18,13 +20,13 @@ public class ListenResultFromServer {
     /*
     Класс постоянно прослушивает сообщения с сервера, чтобы поймать сообщение о входящем рузльтате подписчика.
      */
-    public static Thread thread;
+    private static Thread thread;
     public void startListen() {
         thread = new Thread(new Runnable() {
             public void run() {
                 int err = 0;
                 while (true) {
-                    listen();
+                    listenServer();
                     try {
                         Thread.sleep(1000);
                     } catch (Exception ex) {
@@ -38,130 +40,167 @@ public class ListenResultFromServer {
         thread.start();
         thread.isInterrupted();
     }
+    public void stopListen() {
+        thread.stop();
+    }
 
-    public static ServerSocket serversock = null;
-    public static Socket socket = null;
-
-    public void listen() {
+    private void listenServer() {
         GiveMeSettings giveMeSettings = new GiveMeSettings();
-        try {
-            int err = 0;
-            wh:
-            while (true) {
+        ServerSocket serversocket = getServerSocket(giveMeSettings);
+        if (serversocket == null)
+            return;
+        while (true) {
+            try {
+                Socket socket = serversocket.accept();
+                startSocketNewAccept(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
                 try {
-                    serversock = new ServerSocket(giveMeSettings.getServerPort(false));
-                } catch (Exception ex) {
-                    System.out.println("Не удалось создать сокет для прослушивания ответов с сервера. Ошибка: " +
-                            ex.getMessage());
-                    Thread.sleep(1000);
-                    err++;
-                    if (err > 9) {
-                        System.out.println("Количество попыток подключения больше 9. " +
-                                "Проверьте настройки приложения.");
-                        Thread.sleep(10000);
-                        break;
-                    }
-                    continue;
+                    serversocket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
                 }
-                while (true) {
-                    socket = serversock.accept();
-                    DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-                    DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                return;
+            }
+        }
+    }
 
-                    int len = 0;
-                    byte[] msgbyte;
-                    do { //запускаем прослушку
-                        msgbyte = new byte[inputStream.available()];
-                        len = inputStream.read(msgbyte);
-                    } while (len == 0); //получили сообщение
-                    //дешефруем все, что после первого байта
-                /*
-                * msgbyte[0] - шифрование
-                * msgbyte[1] - тип сообщения
-                * msgbyte[2] - длинна логина
-                * msgbyte[3] - логин
-                 * msgbyte[3+msgbyte[2]] - длинна ссылки
-                 */
-                    if (msgbyte[1] != 4) {
-                        System.out.println("Получили левое сообщение. Продолжаем прослушку.");
-                        continue;
-                    }
-                    if (msgbyte[2] < 1) {
-                        System.out.println("Сообщение неверно дешефровано или отправлено. " +
-                                "Длинна логина указана как отрицательная. Продолжаем прослушку.");
-                        continue;
-                    }
+    /*получение нового сокета для входящих соединенний*/
+    private ServerSocket getServerSocket(GiveMeSettings giveMeSettings) {
+        int err = 0;
+        while (true) {
+            try {
+                return new ServerSocket(giveMeSettings.getServerPort(3));
+            } catch (Exception ex) {
+                System.out.println("Не удалось создать сокет для прослушивания ответов с сервера. Ошибка: " +
+                        ex.getMessage());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                err++;
+                if (err > 9) {
+                    System.out.println("Количество попыток подключения больше 9. " +
+                            "Проверьте настройки приложения.");
                     try {
-                        socket.close();
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-
-                    String login = new String(msgbyte, 3, msgbyte[2], "UTF-8");
-
-                    System.out.println("Посмотреть результат пользователя - " + login + "? (yes/no)");
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    alert.setTitle("Пришел результат");
-                    alert.setHeaderText("У пользователя " + login + " новый результат");
-                    alert.setContentText("Хотите посмотреть на результат '" + login + "'?");
-                    Optional<ButtonType> result = alert.showAndWait();
-                    if (result.get() == ButtonType.OK) {
-                        System.out.println("Пользователь согласился посмотреть результат от " + login);
-                    } else if (result.get() == ButtonType.CANCEL) {
-                        continue;
-                    }
-
-                    //Мы получили от пользователя разрешение посмотреть на результат запрос от пользователя
-                    int jb = 3 + msgbyte[2];
-                /*и так мы получили список ссылок разделенных проблем.
-                * Начинаем его обрабатывать и потом передать в систему выдачи */
-                    if (jb >= len) {
-                        System.out.println("Результат пользователя пуст");
-                        alert = new Alert(Alert.AlertType.INFORMATION);
-                        alert.setTitle("Информация");
-                        alert.setHeaderText("");
-                        alert.setContentText("Результат " + login + " пуст");
-                        alert.showAndWait();
-                        break;
-                    }
-                    ArrayList<String> links = new ArrayList<String>();
-                    while (jb < len) {
-                        int size = ByteBuffer.wrap(msgbyte, jb, 4).getInt();
-                        jb += 4;
-                        String link = new String(msgbyte, jb, size, "UTF-8");
-                        link = getIdFromLink(link);
-                        if (link != null)
-                            links.add(link);
-                        jb += size;
-                    }
-                    //Вызов метода подсистемы подзгрузки из соц сетей, пока его нет просто печатаем
-                    for (int i = 0; i < links.toArray().length; i++) {
-                        System.out.println("Ссылка по запросу пользователя (" + login + "): " + links.get(i));
-                    }
-                    //GetSomePrivateData getSomePrivateData = new GetSomePrivateData();
-                    //getSomePrivateData.vkGet(links, giveMeSettings.getSocialStg());
+                    return null;
                 }
             }
-        } catch (Exception ex) {
-            System.out.println("Возникла ошибка при прослушивании данных");
         }
-        try {
-            socket.close();
-        } catch (Exception ignored) {
-        }
-        try {
-            serversock.close();
-        } catch (Exception ignored) {
-        }
-        //конец метода прослушки
     }
-    private String getIdFromLink(String link)
+
+    /*метод обработки нового соединения*/
+    private boolean startSocketNewAccept(Socket socket) {
+        try {
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            int len = 0;
+            byte[] msg = new byte[0];
+            while (len == 0) { //запускаем прослушку
+                msg = new byte[inputStream.available()];
+                len = inputStream.read(msg);
+            }
+            msgPostsProcessing(msg, len);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    private boolean getResDialogWindow(int what, String login) {
+        if (what == 1) {
+            System.out.println("Посмотреть результат пользователя - " + login + "? (yes/no)");
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Пришел результат");
+            alert.setHeaderText("У пользователя " + login + " новый результат");
+            alert.setContentText("Хотите посмотреть на результат '" + login + "'?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                System.out.println("Пользователь согласился посмотреть результат от " + login);
+                return true;
+            } else
+                return false;
+        } else if (what == 2) {
+            System.out.println("Результат пользователя пуст");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Информация");
+            alert.setHeaderText("");
+            alert.setContentText("Результат " + login + " пуст");
+            alert.showAndWait();
+            return true;
+        } else if (what == 3) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Информация");
+            alert.setHeaderText("Ошибка входа");
+            alert.setContentText("Неправильный логин или пароль");
+            alert.showAndWait();
+            return true;
+        }
+        return false;
+    }
+    private void msgPostsProcessing(byte[] msg, int len)
     {
+        //дешифруем
+        if (msg[1] == (byte)102) {
+            System.out.println("Неправильный логин или пароль. Тип: " + msg[1]);
+            getResDialogWindow(3,null);
+        } else if (msg[1] != 2) {
+            System.out.println("Получили левое сообщение. Продолжаем прослушку.");
+        } else if (msg[2] < 1) {
+            System.out.println("Сообщение неверно дешефровано или отправлено. " +
+                    "Длинна логина указана как отрицательная. Продолжаем прослушку.");
+        } else {
+            try {
+                String login = new String(msg, 3, msg[2], "UTF-8");
+                if (!getResDialogWindow(1,login))
+                    return;
+                formationListLinks(msg,len,login);
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void formationListLinks(byte[] msg, int len, String login) {
+        //Мы получили от пользователя разрешение посмотреть на результат запрос от пользователя
+        int jb = 3 + msg[2];
+                /*и так мы получили список ссылок разделенных проблем.
+                * Начинаем его обрабатывать и потом передать в систему выдачи */
+        if (jb >= len) {
+            getResDialogWindow(2,null);
+        }
+        ArrayList<String> links = new ArrayList<String>();
+        while (jb < len) {
+            int size = ByteBuffer.wrap(msg, jb, 4).getInt();
+            jb += 4;
+            try {
+                String link = null;
+                link = new String(msg, jb, size, "UTF-8");
+                link = getIdFromLink(link);
+                if (link != null)
+                    links.add(link);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            jb += size;
+        }
+        for (int i = 0; i < links.toArray().length; i++) {
+            System.out.println("Ссылка по запросу пользователя (" + login + "): " + links.get(i));
+        }
+        //GetSomePrivateData getSomePrivateData = new GetSomePrivateData();
+        //getSomePrivateData.vkGet(links, giveMeSettings.getSocialStg());
+    }
+
+    private String getIdFromLink(String link) {
         String tmp = null;
-        if (link.toCharArray()[link.length()-1] == '/')
-            link = link.substring(0,link.length()-2);
-        for (int i = link.length()-1; i >=0 ; i--)
-        {
+        if (link.toCharArray()[link.length() - 1] == '/')
+            link = link.substring(0, link.length() - 2);
+        for (int i = link.length() - 1; i >= 0; i--) {
             tmp = link.toCharArray()[i] + tmp;
         }
         return tmp;
