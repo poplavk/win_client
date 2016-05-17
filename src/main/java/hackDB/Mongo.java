52b00b6c6412902a50a827095ad8656bdef2ea5f
SAmser93 <samser_krut93@mail.ru>
2016-05-17 23:27:08 +0300

    Add files via upload

---
+++ src/main/java/hackDB/Mongo.java
@@ -0,0 +1,167 @@
+package ru.mephi.hackDB;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.google.gson.reflect.TypeToken;
+import com.mongodb.*;
+
+import com.google.gson.*;
+
+/**
+ * Created by Bill Gates on 15.05.2016.
+ */
+public class Mongo {
+    DB db;
+    MongoClient mongoClient;
+    String nameParam;
+    String nameField;
+    String lnameField;
+    String linkField;
+    String picField;
+    ArrayList<String> GetPhoto(String name, String Sname, String Colname)
+    {
+        ArrayList<String> list= new ArrayList<>();
+        DBCollection coll = db.getCollection(Colname);
+        BasicDBObject query = new BasicDBObject(nameParam+"."+nameField, new BasicDBObject("$eq", name))
+                .append(nameParam+"."+lnameField, new BasicDBObject("$eq", Sname));
+        DBCursor cursor = coll.find(query);
+        if(cursor.size()>0) {
+            try {
+                while (true) {
+                    JsonParser parser = new JsonParser();
+                    JsonObject obj = parser.parse(cursor.next().toString()).getAsJsonObject();
+                    JsonPrimitive jobj = new Gson().fromJson(obj.get(picField), JsonPrimitive.class);
+                    String res = jobj.getAsString();
+                    list.add(res);
+                    if(!cursor.hasNext()) break;
+                }
+                cursor.close();
+                return list;
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else {
+            System.out.println("Такого человека нет в базе");
+            return null;
+        }
+    }
+    ArrayList<String> GetLink(String name, String Sname, String Colname)
+    {
+        ArrayList<String> list= new ArrayList<>();
+        DBCollection coll = db.getCollection(Colname);
+        BasicDBObject query = new BasicDBObject(nameParam+"."+nameField, new BasicDBObject("$eq", name))
+                .append(nameParam+"."+lnameField, new BasicDBObject("$eq", Sname));
+        DBCursor cursor = coll.find(query);
+        if(cursor.size()>0) {
+            try {
+                while (true) {
+                    JsonParser parser = new JsonParser();
+                    JsonObject obj = parser.parse(cursor.next().toString()).getAsJsonObject();
+                    JsonPrimitive jobj = new Gson().fromJson(obj.get(linkField), JsonPrimitive.class);
+                    String res = jobj.getAsString();
+                    list.add(res);
+                    if(!cursor.hasNext()) break;
+                }
+                cursor.close();
+                return list;
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else {
+            System.out.println("Такого человека нет в базе");
+            return null;
+        }
+    }
+    ArrayList<String> GetName(String Link, String Colname)
+    {
+        ArrayList<String> list= new ArrayList<>();
+        DBCollection coll = db.getCollection(Colname);
+        BasicDBObject query = new BasicDBObject(linkField, new BasicDBObject("$eq", Link));
+        DBCursor cursor = coll.find(query);
+        if(cursor.size()>0) {
+            try {
+                while (true) {
+                    JsonParser parser = new JsonParser();
+                    JsonObject obj = parser.parse(cursor.next().toString()).getAsJsonObject();
+                    JsonObject jobj = new Gson().fromJson(obj.get(nameParam), JsonObject.class);
+                    String res = jobj.get(nameField).getAsString() + jobj.get(lnameField).getAsString();
+                    list.add(res);
+                    if(!cursor.hasNext()) break;
+                }
+                cursor.close();
+                return list;
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else {
+            System.out.println("Такого человека нет в базе");
+            return null;
+        }
+    }
+    ArrayList<String> GetData(String name, String Sname, String Colname)
+    {
+        Gson gson = new Gson();
+        ArrayList<String> list= new ArrayList<>();
+        Map<String, Object> map;
+        DBCollection coll = db.getCollection(Colname);
+        BasicDBObject query = new BasicDBObject(nameParam+"."+nameField, new BasicDBObject("$eq", name))
+                .append(nameParam+"."+lnameField, new BasicDBObject("$eq", Sname));
+        DBCursor cursor = coll.find(query);
+        if(cursor.size()>0) {
+            try {
+                while (true) {
+                    map = gson.fromJson(cursor.next().toString(), new TypeToken<Map<String, Object>>(){}.getType());
+                    map.forEach((x,y)-> list.add(y.toString()));
+                    if(!cursor.hasNext()) break;
+                }
+                cursor.close();
+                return list;
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else {
+            System.out.println("Такого человека нет в базе");
+            return null;
+        }
+    }
+    boolean DBConnect(String address,int port,String DBNAme)
+    {
+        try {
+            mongoClient = new MongoClient(address , port );
+            db = mongoClient.getDB( DBNAme );
+            return true;
+        }catch(Exception ex){
+            System.out.println("Неправильно указаны данные сервера БД. Прерывание работы");
+            return false;
+        }
+    }
+    public void CloseConn()
+    {
+        try
+        {
+            if (mongoClient != null)
+            {
+                mongoClient.close();
+                System.out.println("Соединение закрыто");
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hackDB/SQL.java
@@ -0,0 +1,172 @@
+package ru.mephi.hackDB;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Bill Gates on 15.05.2016.
+ */
+public class SQL implements DBI {
+    Connection conn = null;
+    String nameField;
+    String lnameField;
+    String linkField;
+    String picField;
+    public ArrayList<String> QuerySQL(String input)
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        if (conn != null) {
+            try {
+                Statement m_Statement = conn.createStatement();
+                ResultSet m_ResultSet = m_Statement.executeQuery(input);
+                ResultSetMetaData rsmd = m_ResultSet.getMetaData();
+                byte cnt;
+                if(rsmd.getColumnCount()>0) {
+                    while (m_ResultSet.next()) {
+                        cnt=1;
+                        while(cnt<=rsmd.getColumnCount()) {
+                            list.add(m_ResultSet.getString(cnt));
+                            cnt++;
+                        }
+                        return list;
+                    }
+                }else{
+                    System.out.println("В базе нет такого человека, либо у него нет нужных данных");
+                    return null;
+                }
+            }catch (SQLException ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else
+        {
+            System.out.println("Невозможно установить связь с базой");
+            return null;
+        }
+        return null;
+    }
+
+    public ArrayList<String> GetPhoto(String name, String Sname, String DBname)
+    {
+        ArrayList<String> list;
+        if (conn != null) {
+            try {
+                String query = "SELECT "+picField+" FROM "+DBname+" "+
+                               "WHERE "+nameField+" = '"+name+"' and "+
+                               lnameField+" = '"+Sname+"'";
+                list=QuerySQL(query);
+                return list;
+            }catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else
+        {
+            System.out.println("Невозможно установить связь с базой");
+            return null;
+        }
+    }
+
+    public ArrayList<String> GetLink(String name, String Sname, String DBname)
+    {
+        ArrayList<String> list;
+        if (conn != null) {
+            try {
+                String query = "SELECT "+linkField+" FROM "+DBname+" "+
+                        "WHERE "+nameField+" = '"+name+"' and "+
+                        lnameField+" = '"+Sname+"'";
+                list=QuerySQL(query);
+                return list;
+            }catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else
+        {
+            System.out.println("Невозможно установить связь с базой");
+            return null;
+        }
+    }
+    public ArrayList<String> GetName(String Link, String DBname)
+    {
+        ArrayList<String> list;
+        if (conn != null) {
+            try {
+                String query = "SELECT "+nameField+","+lnameField+" FROM "+DBname+" "+
+                        "WHERE "+linkField+" = '"+Link+"'";
+                list=QuerySQL(query);
+                return list;
+            }catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else
+        {
+            System.out.println("Невозможно установить связь с базой");
+            return null;
+        }
+    }
+    public ArrayList<String> GetData(String name, String Sname, String DBname)
+    {
+        ArrayList<String> list;
+        if (conn != null) {
+            try {
+                String query = "SELECT * FROM "+DBname+" "+
+                        "WHERE "+nameField+" = '"+name+"' and "+
+                        lnameField+" = '"+Sname+"'";
+                list=QuerySQL(query);
+                return list;
+            }catch (Exception ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+        else
+        {
+            System.out.println("Невозможно установить связь с базой");
+            return null;
+        }
+    }
+    public boolean DBConnect(String address,int port,String DBNAme, String user, String pass) {
+        try
+        {
+            conn = DriverManager.getConnection("jdbc:sqlserver://"+address+":"+port+";DatabaseName="+DBNAme,user,pass);
+            if (conn != null)
+            {
+                System.out.println("Соединение установлено. Можно работать");
+                return true;
+            }
+        }
+        catch (SQLException ex)
+        {
+            //ex.printStackTrace();
+            System.out.println("Неправильно указаны данные сервера БД. Прерывание работы");
+            return false;
+        }
+        return false;
+    }
+    public void CloseConn()
+    {
+        try
+        {
+            if (conn != null && !conn.isClosed())
+            {
+                conn.close();
+                System.out.println("Соединение закрыто");
+            }
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
