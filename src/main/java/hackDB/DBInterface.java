c7c373b1644d2d77f1c372cc1d259758cd762f7c
SAmser93 <samser_krut93@mail.ru>
2016-06-16 14:30:53 +0300

    Update DBInterface.java

--- src/main/java/hackDB/DBInterface.java
+++ src/main/java/hackDB/DBInterface.java
@@ -3,18 +3,25 @@ package hackDB;
 import java.util.ArrayList;
 
 /**
- * Created by Bill Gates on 13.05.2016.
+ * Created by Suharev on 15.05.2016.
+ * Интерфейс для методов, работающих с внешними базами данных
+ * Содержит в себе 4 метода для формирования нужных запросов (подробнее описаны в соотв. наследуемых классах),
+ * метод для установки соединения с БД
+ * и метод для разрыва соединения
  */
+ 
 public interface DBInterface {
+    //Формирование запросов
+    //Фотография по ФИО
     ArrayList<String> GetPhoto(String a, String b, String c);
-
+    //Ссылка по ФИО
     ArrayList<String> GetLink(String a, String b, String c);
-
+    //ФИО по ссылке
     ArrayList<String> GetName(String a, String b);
-
+    //Все данные по ФИО
     ArrayList<String> GetData(String a, String b, String c);
-
+    //Установка соединения
     boolean DBConnect(String address, int port, String DBNAme, String user, String pass);
-
+    //Разрыв соединения
     void CloseConn();
 }
