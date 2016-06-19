0f148be10b0c3c9166a7ec310e9d682c21ea7f39
VDelOff <delov_93@mail.ru>
2016-06-19 17:35:03 +0300

    Update MyImage.java

--- src/main/java/delov/MyImage.java
+++ src/main/java/delov/MyImage.java
@@ -14,19 +14,29 @@ import org.opencv.videoio.VideoCapture;
 
 import java.io.ByteArrayInputStream;
 
-
+/**
+ * Класс для хранения и обработки изображения,
+ * из которого получают эталон
+ */
 public class MyImage extends ImageView implements EventHandler <MouseEvent> {
 
+    // текущие координаты позиции курсора мыши на экране приложения
     private double current_x;
     private double current_y;
-
+    // реализация каскадного классификатора Хаара
     private CascadeClassifier faceCascade;
 
+    // комплекснозначная матрица изображения
     Mat tmp;
     Mat tmp1;
+    // комплекснозначная матрица изображения выбранного лица
     Mat subtmp;
+    // массив комплекснозначных матрица для распознанных лиц
     MatOfRect faces;
 
+    /**
+     * Конструктор класса MyImage
+     */
     public MyImage() {
         current_x = 0;
         current_y = 0;
@@ -35,27 +45,49 @@ public class MyImage extends ImageView implements EventHandler <MouseEvent> {
         faceCascade.load("./resources/haarcascades/haarcascade_frontalface_alt.xml");
     }
 
+    /**
+     * Метод, устанавливающий в текущий класс следующие параметры:
+     * @param tmp Mat матрица исходного изображения
+     * @param faces массив Mat матриц распознанных лиц
+     */
     public void setMapIm(Mat tmp, MatOfRect faces) {
 
         this.tmp=tmp;
         this.faces=faces;
     }
 
+    /**
+     * Метод, возвращающий Mat матрицу выбранного лица
+     * @return subtmp Mat матрица выбранного лица
+     */
     public Mat getMapIm() {
 
         return subtmp;
     }
 
+    /**
+     * Метод, возвращающий Mat матрицу исходного изображения
+     * @return tmp1 Mat матрица исходного изображения
+     */
     public Mat getMapTmp() {
 
         return tmp1;
     }
 
+    /**
+     * Метод, обнуляющий Mat матрицу изображения выбранного лица
+     */
     public void setNullMapIm() {
 
         subtmp=null;
     }
 
+    /**
+     * Метод, срабатывающий по нажатию мыши на изображении.
+     * Если курсор был внутри рамки с выделенным лицом, текущее изображение
+     * заменяется на изображение с выделенным лицом
+     * @param e событие мыши, которое нужно обработать
+     */
     public void handle(MouseEvent e) {
         if (e.getEventType() == MouseEvent.MOUSE_CLICKED) {
             current_x = e.getX();
@@ -71,7 +103,7 @@ public class MyImage extends ImageView implements EventHandler <MouseEvent> {
                 double d = rect.y / ((double)tmp.width()/ 400) + rect.height / ((double)tmp.width() / 400);
                 if ((current_x >= a) && (current_y >= b) && (current_x <= c) && (current_y <= d)) {
                     subtmp=tmp.submat(rect);
-                    this.setImage(mat2Image(tmp.submat(rect)));//new Image(file.toURI().toString()));
+                    this.setImage(mat2Image(tmp.submat(rect)));
                     this.setFitWidth(400);
                     this.setPreserveRatio(true);
                     this.setSmooth(true);
@@ -83,39 +115,31 @@ public class MyImage extends ImageView implements EventHandler <MouseEvent> {
     }
 
     /**
-     * Get a frame from the opened video stream (if any)
-     *
-     * @return the {@link Image} to show
+     * Метод, который получает видеокадр из видеопотока, преобразует его в Mat матрицу,
+     * обрабатывает её, и затем ковертирует её в изображение
+     * @param capture кадр видеопотока
+     * @return imageToShow - обработанное изображение, которое будет выведено на экран.
      */
     public  Image grabFrame(VideoCapture capture)
     {
-        // init everything
         Image imageToShow = null;
         Mat frame = new Mat();
 
-        // check if the capture is open
         if (capture.isOpened())
         {
             try
             {
-                // read the current frame
                 capture.read(frame);
-
-                // if the frame is not empty, process it
                 if (!frame.empty())
                 {
-                    // face detection
                     tmp1=frame;
                     tmp1 = this.detectAndDisplay(tmp1);
-
-                    // convert the Mat object (OpenCV) to Image (JavaFX)
                     imageToShow = mat2Image(frame);
                 }
 
             }
             catch (Exception e)
             {
-                // log the (full) error
                 System.err.println("ERROR: " + e);
             }
         }
@@ -124,32 +148,29 @@ public class MyImage extends ImageView implements EventHandler <MouseEvent> {
     }
 
     /**
-     * Convert a Mat object (OpenCV) in the corresponding Image for JavaFX
-     *
-     * @param frame
-     *            the {@link Mat} representing the current frame
-     * @return the {@link Image} to show
+     * Преобразование Mat объекта (OpenCV) в соответствующее изображение для JavaFX
+     * @param frame представленный текущий кадр
+     * @return Image созданное изображение
      */
-
     public Image mat2Image(Mat frame)
     {
-        // create a temporary buffer
         MatOfByte buffer = new MatOfByte();
-        // encode the frame in the buffer, according to the PNG format
         Imgcodecs.imencode(".png", frame, buffer);
-        // build and return an Image created from the image encoded in the
-        // buffer
         return new Image(new ByteArrayInputStream(buffer.toArray()));
     }
 
+    /**
+     * Метод, который распознает на Mat объекте лица, выделяет их в рамку,
+     * и возвращает обработанный Mat объект
+     * @param tmp Mat матрица исходного изображения
+     * @return tmp Mat матрица исходного изображения с выделенными лицами
+     */
     public  Mat detectAndDisplay(Mat tmp)  {
 
         MatOfRect faces = new MatOfRect();
 
-        // detect faces
         this.faceCascade.detectMultiScale(tmp,faces);
         this.setMapIm(tmp, faces);
-        // each rectangle in faces is a face: draw them!
         for (Rect rect : faces.toArray()) {
             rect.y = rect.y / 10 * 8;
             rect.height = rect.height / 10 * 14;
